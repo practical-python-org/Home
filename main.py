acf391f6094e304aef1c2921879721f093ef051b
ManosThePug <150543985+ManosThePug@users.noreply.github.com>
2023-11-15 15:26:04 +0200

    New_Website

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', '[POST'])
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/FAQ")
+def faq():
+    return render_template("faq.html")
+
+@app.route("/features")
+def features():
+    return render_template("features.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# Websites URL: http://127.0.0.1:5000
