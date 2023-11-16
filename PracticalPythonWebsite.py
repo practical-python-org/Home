7ad5c3218f4f1d3594490ad589371be223badf1c
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-11-16 20:46:18 +0100

    Added wsgi entry point

--- PracticalPythonWebsite.py
+++ PracticalPythonWebsite.py
@@ -23,8 +23,7 @@ def faq():
 def features():
     return render_template("features.html")
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0')
 
 # Websites URL: http://127.0.0.1:8080
